d6c582be1a7b310de683933efcdef3d0e8008686
권누리 <70535179+noory-kwon@users.noreply.github.com>
2023-11-22 17:53:10 +0900

    Create index.js

---
+++ node_react_definition/index.js
@@ -0,0 +1,25 @@
+const calc = require("./calc"); // 내보낸 모듈 가져오기
+
+console.log(calc);
+
+/* 
+Node.js
+: 프로젝트를 통해 탄생
+크롬의 자바스크립트 엔진인 V8엔진을 이용하여 자바스크립트를 브라우저가 아닌 어디에서든 사용할 수 있도록 만들어줌
+자바스크립트의 실행환경(runtime) -> JavaScript로 webserver 개발가능
+
+
+리액트
+: JavaScript 라이브러리 중 하나로, 여러 기능의 자바스크립트 파일들을 쉽게 만들어낼 수 있음
+
+
+npm (Node Package Manager)
+: Node.js의 패키지 관리 도구
+다른사람이 만들어 놓은 모듈들(패키지)을 내려받아서 사용할 수 있음
+개발한 프로젝트 관리
+
+*/
+
+
+// 간격 예쁘게 포매팅 정리해주는 확장 프로그램 설치 : prettier 설치
+// 파일 아이콘 바꿔주는 플러그인 : material icon 설치
