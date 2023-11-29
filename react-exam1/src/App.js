91a8cd08d08258e93325600ed763076fa52b54d9
taja <noorykwon@gmail.com>
2023-11-29 17:52:44 +0900

    React.js 기초 - JSX

--- react-exam1/src/App.js
+++ react-exam1/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import './App.css'; //css를 외부에 위치하는 방식
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+
 
 function App() {
+  let name = "타야"
+
+  const style = {  //css를 내부에 위치하는 방식
+    App : {
+      backgroundColor: 'black',
+    },
+    h2 : {
+      color : "red"
+    },
+    bold_text : {
+      color : "green"
+    }
+  }
+
+  const number = Math.floor(Math.random()*10);
+
   return (
-    <div className="App">
+    <div style={style.App}>
+      <MyHeader />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={style.h2}>안녕 리액트</h2>
+        <b id="bold_text" style={style.bold_text}>
+          React!! <br />
+        {number}는 : {number % 2 === 0? "짝수" : "홀수"}
+        </b>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; // es모듈 시스템 (module.exports={} / 이건 common js 모듈 시스템)
+
+/*
+import React from "react"
+
+</React.Fragment> 태그로 최상위 하나의 태그로 묶는 역할을 할 수 있음
+
+빈태그도 가능
+*/
